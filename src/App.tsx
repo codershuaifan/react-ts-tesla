e6f933294a01a0f2d68dece8125ce35c94406385
admin <admin@admindeMacBook-Pro-5.local>
2022-07-13 20:02:10 +0800

    全屏功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Main from './components/main'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+
 
 export default function App() {
+  const handle = useFullScreenHandle();
+  useEffect(() => {
+    handle.enter()
+  }, [])
   return (
     <HashRouter>
-      <Main />
+      <FullScreen handle={handle}>
+        <Main />
+      </FullScreen>
     </HashRouter>
   )
 }
