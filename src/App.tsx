f6cd77b74eb083243cdf29fd08cf85a458053c7a
admin <admin@admindeMacBook-Pro-5.local>
2022-07-13 20:37:58 +0800

    回退全屏功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React, { useEffect, createContext } from 'react'
+import React from 'react'
 import Main from './components/main'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
-import { FullScreen, useFullScreenHandle } from "react-full-screen";
-
-const FullScData = createContext<any>(null)
 
 export default function App() {
-  const handle = useFullScreenHandle();
-  useEffect(() => {
-    handle.enter()
-  }, [])
   return (
     <HashRouter>
-      <FullScreen handle={handle}>
-        <FullScData.Provider value={handle}>
-          <Main />
-        </FullScData.Provider>
-      </FullScreen>
+      <Main />
     </HashRouter>
   )
 }
-
-export { FullScData }
